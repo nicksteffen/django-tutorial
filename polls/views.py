cf0b1676c747deb248526544d11c4b465e3d8015
Nick Steffen <nicksteffen94@gmail.com>
2016-08-14 11:23:12 -0400

    fixed argument

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,8 @@ def index(request):
     
 def detail(request, question_id):
     question = get_object_or_404(Question, pk= question_id)
-    return render(render, 'polls/detail.html',{'question':question})
+    return render(request, 'polls/detail.html',{'question':question})
+
 def results(request, question_id):
     response = "You're looking at the results for question %s"
     return HttpResponse(response % question_id) 
