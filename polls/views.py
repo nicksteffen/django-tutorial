1cc017fb62933bf74c66532e38680f3cf09070fe
Nick Steffen <nicksteffen94@gmail.com>
2016-08-14 11:22:44 -0400

    include import

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.template import loader
 from .models import Question
