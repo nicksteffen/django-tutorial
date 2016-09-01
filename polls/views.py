11d309ced688f686efbe0460e4914828ffd9bda7
nick steffen <nick steffen>
2016-09-01 19:33:55 -0400

    filled out vote function

--- polls/views.py
+++ polls/views.py
@@ -21,4 +21,17 @@ def results(request, question_id):
     return HttpResponse(response % question_id) 
 
 def vote(request, question_id):
-    return HttpResponse("Youre voting on question %s" %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError Choice.DoesNotExist):
+        return render(request 'polls/details.html',{
+            'question':question, 'error_message': 'You forgot to select a question'
+        })
+    else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
