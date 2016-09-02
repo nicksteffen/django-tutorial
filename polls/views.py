01c3de826fd034c245a26a8b723325d8f1f1c97d
nick steffen <nick steffen>
2016-09-01 20:30:37 -0400

    import Choice

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 from django.http import HttpResponse
