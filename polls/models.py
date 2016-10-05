5cce32cdaa61392df2466d1fb31d720407943eda
nick steffen <nick steffen>
2016-10-04 21:12:09 -0400

    Added Question Admin

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        #return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
+    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
